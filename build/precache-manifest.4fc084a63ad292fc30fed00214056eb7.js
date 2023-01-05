9225c041cc70fac92794fda444637cf4c83bd904
amitkkumra <amitkkumra@outlook.com>
2023-01-05 14:59:58 +0000

    Added the build code

---
+++ build/precache-manifest.4fc084a63ad292fc30fed00214056eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9b06ce636a1206d480145f0e8deeea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee2436d6dbbd46992b0b",
+    "url": "/static/css/main.b7af7d1a.chunk.css"
+  },
+  {
+    "revision": "650d9f1e47f0834c4fd1",
+    "url": "/static/js/2.9e3ee35f.chunk.js"
+  },
+  {
+    "revision": "ee2436d6dbbd46992b0b",
+    "url": "/static/js/main.c25e726d.chunk.js"
+  },
+  {
+    "revision": "5937e05f753a6b0befdc",
+    "url": "/static/js/runtime~main.43de2fcf.js"
+  }
+]);
